6aaa2c1d7d0b6881f875ba802f18e6bee82db35a
vahidtaslimi <vt@Vahids-Mac.local>
2014-03-29 22:31:11 +1100

    added dropbox sdk.

--- yekdrive/yekdrive/ELVAppDelegate.h
+++ yekdrive/yekdrive/ELVAppDelegate.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <DropboxSDK/DropboxSDK.h>
 
-@interface ELVAppDelegate : UIResponder <UIApplicationDelegate>
+@interface ELVAppDelegate : UIResponder <UIApplicationDelegate,DBSessionDelegate, DBNetworkRequestDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property (nonatomic, retain) IBOutlet UIViewController *rootViewController;
 
 @end
--- yekdrive/yekdrive/ELVAppDelegate.m
+++ yekdrive/yekdrive/ELVAppDelegate.m
@@ -7,8 +7,12 @@
 //
 
 #import "ELVAppDelegate.h"
+#import <DropboxSDK/DropboxSDK.h>
 
 @implementation ELVAppDelegate
+{
+    	NSString *relinkUserId;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -18,9 +22,85 @@
         UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
         splitViewController.delegate = (id)navigationController.topViewController;
     }
+    NSString* appKey = @"sbyt9gkp1tusxgh";
+	NSString* appSecret = @"lt8ntu78x1d4lm5";
+	NSString *root =kDBRootDropbox;// @"dropbox"; // Should be set to either kDBRootAppFolder or kDBRootDropbox
+	// You can determine if you have App folder access or Full Dropbox along with your consumer key/secret
+	// from https://dropbox.com/developers/apps
+	
+	// Look below where the DBSession is created to understand how to use DBSession in your app
+	
+	NSString* errorMsg = nil;
+	if ([appKey rangeOfCharacterFromSet:[[NSCharacterSet alphanumericCharacterSet] invertedSet]].location != NSNotFound) {
+		errorMsg = @"Make sure you set the app key correctly in DBRouletteAppDelegate.m";
+	} else if ([appSecret rangeOfCharacterFromSet:[[NSCharacterSet alphanumericCharacterSet] invertedSet]].location != NSNotFound) {
+		errorMsg = @"Make sure you set the app secret correctly in DBRouletteAppDelegate.m";
+	} else if ([root length] == 0) {
+		errorMsg = @"Set your root to use either App Folder of full Dropbox";
+	} else {
+		NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+		NSData *plistData = [NSData dataWithContentsOfFile:plistPath];
+		NSDictionary *loadedPlist =
+        [NSPropertyListSerialization
+         propertyListFromData:plistData mutabilityOption:0 format:NULL errorDescription:NULL];
+		NSString *scheme = [[[[loadedPlist objectForKey:@"CFBundleURLTypes"] objectAtIndex:0] objectForKey:@"CFBundleURLSchemes"] objectAtIndex:0];
+		if ([scheme isEqual:@"db-APP_KEY"]) {
+			errorMsg = @"Set your URL scheme correctly in DBRoulette-Info.plist";
+		}
+	}
+	
+	DBSession* session =
+    [[DBSession alloc] initWithAppKey:appKey appSecret:appSecret root:root];
+	session.delegate = self; // DBSessionDelegate methods allow you to handle re-authenticating
+	[DBSession setSharedSession:session];
+ 
+	
+	[DBRequest setNetworkRequestDelegate:self];
+    
+	if (errorMsg != nil) {
+		[[[UIAlertView alloc]
+		   initWithTitle:@"Error Configuring Session" message:errorMsg
+		   delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]
+		  
+		 show];
+	}
+    
+   /* rootViewController.photoViewController = [[PhotoViewController new] autorelease];
+    if ([[DBSession sharedSession] isLinked]) {
+        navigationController.viewControllers =
+        [NSArray arrayWithObjects:rootViewController, rootViewController.photoViewController, nil];
+    }
+    */
+    
+    // Add the navigation controller's view to the window and display.
+    /*[window addSubview:navigationController.view];
+    [window makeKeyAndVisible];
+	*/
+	NSURL *launchURL = [launchOptions objectForKey:UIApplicationLaunchOptionsURLKey];
+	NSInteger majorVersion =
+    [[[[[UIDevice currentDevice] systemVersion] componentsSeparatedByString:@"."] objectAtIndex:0] integerValue];
+	if (launchURL && majorVersion < 4) {
+		// Pre-iOS 4.0 won't call application:handleOpenURL; this code is only needed if you support
+		// iOS versions 3.2 or below
+		[self application:application handleOpenURL:launchURL];
+		return NO;
+	}
+    
     return YES;
+
 }
-							
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+	if ([[DBSession sharedSession] handleOpenURL:url]) {
+		if ([[DBSession sharedSession] isLinked]) {
+			//[navigationController pushViewController:rootViewController.photoViewController animated:YES];
+		}
+		return YES;
+	}
+	
+	return NO;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -48,4 +128,47 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+#pragma mark -
+#pragma mark DBSessionDelegate methods
+
+- (void)sessionDidReceiveAuthorizationFailure:(DBSession*)session userId:(NSString *)userId {
+	relinkUserId = userId ;
+	[[[UIAlertView alloc]
+	   initWithTitle:@"Dropbox Session Ended" message:@"Do you want to relink?" delegate:self
+	   cancelButtonTitle:@"Cancel" otherButtonTitles:@"Relink", nil]
+	  
+	 show];
+}
+
+
+#pragma mark -
+#pragma mark UIAlertViewDelegate methods
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)index {
+	if (index != alertView.cancelButtonIndex) {
+		/*[[DBSession sharedSession] linkUserId:relinkUserId fromController:rootViewController];*/
+	}
+
+	relinkUserId = nil;
+}
+
+
+#pragma mark -
+#pragma mark DBNetworkRequestDelegate methods
+
+static int outstandingRequests;
+
+- (void)networkRequestStarted {
+	outstandingRequests++;
+	if (outstandingRequests == 1) {
+		[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+	}
+}
+
+- (void)networkRequestStopped {
+	outstandingRequests--;
+	if (outstandingRequests == 0) {
+		[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+	}
+}
 @end
